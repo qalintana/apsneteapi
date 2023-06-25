adaf4ecbf7b50976fabd791b91382ae51c6127b9
Queiroz Santana <alintana18@gmail.com>
2023-06-25 10:35:56 +0100

    chore: Api wth aspnet

---
+++ Controller/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_EF.Controller;
+
+[Route("categories")]
+public class CategoryController : ControllerBase
+{
+
+    [Route("")]
+    [HttpGet]
+    public string MeuTodo()
+    {
+        return "Olá Mundo";
+    }
+
+    [Route("{id:int}")]
+    [HttpGet]
+    public string GetById(int id)
+    {
+        return $"Meu sistema todo funcional {id}";
+    }
+
+    [Route("")]
+    [HttpPost]
+    public Category Post([FromBody] Category model)
+    {
+        return model;
+    }
+
+
+    [Route("{id:int}")]
+    [HttpPut]
+    public Category? Put(int id, [FromBody] Category model)
+    {
+        if (model.Id == id)
+        {
+            return model;
+        }
+        return null;
+    }
+
+
+    [Route("{id:int}")]
+    [HttpDelete]
+    public string Delete()
+    {
+        return "DELETE";
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_EF;
+
+public class User
+{
+    
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Este campo é obrigatorio")]
+    [MaxLength(20, ErrorMessage = "Este campo deve conter no minimo 3 e 60 no maximo")]
+    [MinLength(3, ErrorMessage = "Este campo deve conter no minimo 3 e 60 no maximo")]
+    public string Username { get; set; }  
+    
+    [Required(ErrorMessage = "Este campo é obrigatorio")]
+    [MaxLength(20, ErrorMessage = "Este campo deve conter no minimo 3 e 60 no maximo")]
+    [MinLength(3, ErrorMessage = "Este campo deve conter no minimo 3 e 60 no maximo")]
+    public string password { get; set; }
+    
+    public string Role { get; set; }
+}
