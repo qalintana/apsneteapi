12e3b0c25f7699476d164c1b6b6b79ce862f15ff
Queiroz Santana <alintana18@gmail.com>
2023-06-26 22:07:12 +0100

    Crud products done

--- Controller/ProductController.cs
+++ Controller/ProductController.cs
@@ -16,17 +16,44 @@ public class ProductController : ControllerBase
         return Ok(products);
     }
 
+
     [HttpGet]
     [Route("categories/{id:int}")]
-    public async Task<ActionResult<Product>> GetById([FromServices] DataContext context, int id)
+    public async Task<ActionResult<Product>> GetBy([FromServices] DataContext context, int id)
     {
-        var product = await context.Products.Include(category => category.category).
-                            AsNoTracking().FirstOrDefaultAsync(product => product.Id == id);
+        var product = await context.Products.Include(c => c.category).
+                            AsNoTracking().FirstOrDefaultAsync(c => c.Id == id);
         if (product != null)
         {
             return Ok(product);
         }
         return BadRequest(new { message = "Produto não encontrado" });
+    }
+    public async Task<ActionResult<Product>> GetByCategory([FromServices] DataContext context, int id)
+    {
+        var product = await context.Products.Include(c => c.category).
+                            AsNoTracking().Where(c => c.CategoryId == id).ToListAsync();
+        if (product != null)
+        {
+            return Ok(product);
+        }
+        return BadRequest(new { message = "Produto não encontrado" });
+    }
+
+    [HttpPost]
+    [Route("")]
+    public async Task<ActionResult<Product>> Post([FromServices] DataContext context, [FromBody] Product model)
+    {
+        if (ModelState.IsValid)
+        {
+            context.Products.Add(model);
+            await context.SaveChangesAsync();
+            return Ok(model);
+        }
+        else
+        {
+            return BadRequest(ModelState)
+        }
     }
 
 }
