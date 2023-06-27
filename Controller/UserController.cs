064549469b5f83ad430e7a896252fbe2a3c848f5
Queiroz Santana <alintana18@gmail.com>
2023-06-27 12:49:35 +0100

    User controller

--- Controller/UserController.cs
+++ Controller/UserController.cs
@@ -1,16 +1,22 @@
 namespace API_EF.Controller;
 
 using API_EF.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 [Route("users")]
 public class UserController : Controller
 {
 
+
+    [HttpPost]
+    [Route("")]
+    [AllowAnonymous]
     public async Task<ActionResult<User>> Post(
-        [FromServices] DataContext context,
-        [FromBody] User model
-    )
+            [FromServices] DataContext context,
+            [FromBody] User model
+        )
     {
         if (!ModelState.IsValid)
         {
@@ -27,5 +33,30 @@ public class UserController : Controller
         {
             return BadRequest(new { message = "Não foi possível criar usuário" });
         }
+    }
+
+    [HttpPost]
+    [Route("login")]
+    public async Task<ActionResult<dynamic>> Authenticate([FromServices] DataContext context,
+            [FromBody] User model)
+    {
+
+        var user = await context.Users.AsNoTracking()
+                .Where(x => x.Username == model.Username && x.password == model.password)
+                .SingleOrDefaultAsync();
+
+        if (user == null)
+        {
+            return BadRequest(new { message = "Usuário ou senha inválidos" });
+        }
+
+        var token = TokenService.GenerateToken(user);
+        return new
+        {
+            user,
+            token
+        };
+
+
     }
 }
